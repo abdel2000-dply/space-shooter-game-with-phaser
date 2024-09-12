0d74e0594cd34333fc3c6f10f7d0979444a595ae
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-09-12 16:44:44 +0100

    Add game scene with player and platform images

--- main.js
+++ main.js
@@ -1,2 +1,47 @@
 import './style.css'
+import Phaser from 'phaser'
 
+const sizes = {
+  width: 400,
+  height: 600
+}
+
+const speed = 240
+
+class GameScene extends Phaser.Scene {
+  constructor() {
+    super("scene-game")
+    this.player
+  }
+
+  preload() {
+    this.load.image('platform', '/assets/images/platform.png')
+    this.load.image('player', '/assets/images/player.png')
+  }
+
+  create() {
+    this.add.image(0, 0, 'platform').setOrigin(0, 0)
+    this.player = this.add.image(0, sizes.height - 100, 'player').setOrigin(0, 0)
+  }
+
+  update() {
+
+  }
+}
+
+const config = {
+  type: Phaser.WEBGL,
+  width: sizes.width,
+  height: sizes.height,
+  canvas:gameCanvas,
+  Physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: speed },
+      debug: true
+    }
+  },
+  scene: [GameScene]
+}
+
+const game = new Phaser.Game(config)
